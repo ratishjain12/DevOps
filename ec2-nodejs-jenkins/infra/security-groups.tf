d09e1b5ef081543f4440dd17d1cd7fab4688d9d7
ratish jain <ratishjain6@gmail.com>
2025-09-14 00:58:42 +0530

    added: egress for jenkins server to ssh into bastion

--- ec2-nodejs-jenkins/infra/security-groups.tf
+++ ec2-nodejs-jenkins/infra/security-groups.tf
@@ -75,6 +75,13 @@ resource "aws_security_group" "jenkins_sg" {
   }
 
   egress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    security_groups = [aws_security_group.bastion_sg.id]
+  }
+
+  egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
