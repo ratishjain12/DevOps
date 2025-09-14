755ce07bc9720adba74d1b292f132a25052c3dca
ratish jain <ratishjain6@gmail.com>
2025-09-15 00:44:16 +0530

    fixes

--- nodejs-elk-ec2/infra/user-data/app.sh
+++ nodejs-elk-ec2/infra/user-data/app.sh
@@ -2,16 +2,26 @@
 
 set -e
 export DEBIAN_FRONTEND=noninteractive
-# Update system
-apt-get update -y
+# Add Docker's official GPG key:
+sudo apt-get update -y
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
 
-# Install basic utilities
-apt install -y curl wget git
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update -y
 
 # Install Docker
-apt install -y docker.io
-systemctl enable docker
-systemctl start docker
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Enable and start Docker
+sudo systemctl enable docker
+sudo systemctl start docker
 usermod -aG docker ubuntu
 
 mkdir -p /opt/nodejs-elk-app
